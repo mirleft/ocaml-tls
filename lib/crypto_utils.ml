c29ecbcd4e043262fd88ff57a026549f3a13c348
pqwy <unitedbiscuits@gmail.com>
2014-02-20 18:09:30 +0000

    small reorg for interactive devel

--- lib/crypto_utils.ml
+++ lib/crypto_utils.ml
@@ -1,3 +1,4 @@
+
 let read_lines filename =
   let chan = open_in filename in
   let rec read () =
@@ -23,32 +24,28 @@ let pem_to_cstruct pem =
 let get_cert_from_file filename =
   let pem = read_pem_file filename in pem_to_cstruct pem
 
-let key =
-  let pem = read_pem_file "server.key" in
+let get_key filename =
+  let pem = read_pem_file filename in
   let str = Cryptokit.(transform_string (Base64.decode ()) pem)
   in
   let Some (pk, _) =
     Asn_grammars.rsa_private_key_of_cstruct (Cstruct.of_string str) in
   String.
     (Printf.printf "got a private key %d %d %d %d %d %d %d %d\n"
-       (length pk.n)
-       (length pk.e)
-       (length pk.d)
-       (length pk.p)
-       (length pk.q)
-       (length pk.dp)
-       (length pk.dq)
-       (length pk.qinv)) ;
+       (length pk.n) (length pk.e ) (length pk.d ) (length pk.p)
+       (length pk.q) (length pk.dp) (length pk.dq) (length pk.qinv));
   pk
 
+let the_key = lazy (get_key "server.key")
+
 let encrypt msg =
-  let crprivate = key in
+  let crprivate = Lazy.force the_key in
   let enc = Cryptokit.RSA.encrypt crprivate msg in
   Printf.printf "enc is %s\n" enc;
   enc
 
 let decrypt msg =
-  let crprivate = key in
+  let crprivate = Lazy.force the_key in
   let dec = Cryptokit.RSA.decrypt crprivate msg in
   Printf.printf "dec is %s\n" dec;
   dec
