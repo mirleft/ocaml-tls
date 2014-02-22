d973ef662fa605ee57e6097ade511b56be844532
Hannes Mehnert <hannes@mehnert.org>
2014-02-22 14:06:02 +0000

    fix hmac stuff

--- lib/flow.ml
+++ lib/flow.ml
@@ -152,22 +152,17 @@ module Server = struct
 
   let empty_state = { machina = `Initial ; decryptor = `Nothing ; encryptor = `Nothing }
 
-  let signature : hash_algorithm -> int64 -> int -> content_type -> string -> string
-    = fun mac n len ty data ->
-            let prefix = Cstruct.create 9 in
+  let signature : hash_algorithm -> int64 -> content_type -> string -> string
+    = fun mac n ty data ->
+            let prefix = Cstruct.create 13 in
+            let len = String.length data in
             Cstruct.BE.set_uint64 prefix 0 n;
-            Cstruct.set_uint8 prefix 4 (Packet.content_type_to_int ty);
-            Cstruct.set_uint8 prefix 5 3; (* version major *)
-            Cstruct.set_uint8 prefix 6 1; (* version minor *)
-            Cstruct.BE.set_uint16 prefix 7 len;
-            let ps = Cstruct.copy prefix 0 9 in
-            let a = Cryptokit.hash_string mac (ps ^ data) in
-            Printf.printf "sig a";
-            Cstruct.hexdump (Cstruct.of_string a);
-            let b = Cryptokit.hash_string mac (ps ^ data) in
-            Printf.printf "sig b";
-            Cstruct.hexdump (Cstruct.of_string b);
-            b
+            Cstruct.set_uint8 prefix 8 (Packet.content_type_to_int ty);
+            Cstruct.set_uint8 prefix 9 3; (* version major *)
+            Cstruct.set_uint8 prefix 10 1; (* version minor *)
+            Cstruct.BE.set_uint16 prefix 11 len;
+            let ps = Cstruct.copy prefix 0 13 in
+            Cryptokit.hash_string mac (ps ^ data)
 
   (* well-behaved pure encryptor *)
   let encrypt_ : crypto_state -> Cstruct.t -> crypto_state * Cstruct.t
@@ -176,8 +171,7 @@ module Server = struct
                  | `Stream (seq, cipher, mac) ->
                     let data = Cstruct.copy buf 0 (Cstruct.len buf) in
                     (* TODO : needs content type!!! *)
-                    let len = (String.length data) + 20 + 1 + 2 + 2 in
-                    let sign = signature mac seq len Packet.HANDSHAKE data in
+                    let sign = signature mac seq Packet.HANDSHAKE data in
                     let enc = Cryptokit.transform_string cipher (data ^ sign) in
                     (`Stream ((Int64.add seq (Int64.of_int 1)), cipher, mac),
                      Cstruct.of_string enc)
@@ -191,14 +185,13 @@ module Server = struct
                     let dec = Cryptokit.transform_string cipher data in
                     Printf.printf "decrypted\n";
                     Cstruct.hexdump (Cstruct.of_string dec);
-                    let declength = String.length dec in
                     let maclength = 20 (* TODO: mac.mac_length *) in
+                    let declength = String.length dec in
                     let macstart = declength - maclength in
                     let body = String.sub dec 0 macstart in
                     let actual_signature = String.sub dec macstart maclength in
                     (* TODO: real content_type *)
-                    let len = declength + 1 + 2 + 2 in
-                    let computed_signature = signature mac seq len Packet.HANDSHAKE body in
+                    let computed_signature = signature mac seq Packet.HANDSHAKE body in
                     Printf.printf "computed signature\n";
                     Cstruct.hexdump (Cstruct.of_string computed_signature);
                     Printf.printf "actual signature\n";
