7fa84c92da879c59e3252359761aa1fb7134aebe
Hannes Mehnert <hannes@mehnert.org>
2014-03-05 15:44:41 +0000

    cleanup and some debug output of certificates

--- lib/certificate.ml
+++ lib/certificate.ml
@@ -12,15 +12,16 @@ type verification_result = [
   | `Ok
 ]
 
-let validate_signature : certificate list -> certificate -> Cstruct.t -> bool =
+let validate_signature : certificate -> certificate -> Cstruct.t -> bool =
   fun trusted c raw ->
     assert (c.signature_algo = c.tbs_cert.signature);
-    (* try to find the public key of the issuer *)
     (* we'll first try the short way and look up certificate authority key identifier from extensions *)
-    let issuing_key = rsa_public_of_cert (List.hd trusted) (*match __ with
+    let issuing_key = rsa_public_of_cert trusted (*match __ with
       | Some x -> x
       | None ->     (* otherwise we use the issuer field *) *)
     in
+    (* we have to check that issuing_key was really the one used to sign this certificate *)
+
     (* XXX: this is awful code! *)
     let siglen = Cstruct.len c.signature in
     (* not sure whether 128 is what we want here, for sure we just want to translate the certificate to a cstruct ;) *)
@@ -39,9 +40,8 @@ let validate_signature : certificate list -> certificate -> Cstruct.t -> bool =
         in
         assert (algo = id_md5);
         let chash = Crypto.md5 to_hash in
-        Printf.printf "hash";
+        Printf.printf "comparing md5";
         Cstruct.hexdump hash;
-        Printf.printf "chash";
         Cstruct.hexdump chash;
         assert (Utils.cs_eq chash hash);
         true
@@ -56,6 +56,9 @@ let validate_signature : certificate list -> certificate -> Cstruct.t -> bool =
           in
           assert (algo = id_sha1);
           let chash = Crypto.sha to_hash in
+          Printf.printf "comparing sha1";
+          Cstruct.hexdump hash;
+          Cstruct.hexdump chash;
           assert (Utils.cs_eq chash hash);
           true
         end
@@ -72,10 +75,45 @@ let validate_time now cert =
   true
 
 let basic_verification now name cert =
+  Printf.printf "basic verify certificate\n";
+  List.iter (fun i -> Printf.printf "ISSUER outer loop\n";
+                      List.iter (fun j -> Printf.printf " inner loop\n";
+                                          let id, name = j in
+                                          Printf.printf "  oid %s val %s\n"
+                                                        (String.concat "." (List.map string_of_int (OID.to_list id)))
+                                                        name)
+                                i)
+            cert.issuer;
+  List.iter (fun i -> Printf.printf "SUBJECT outer loop\n";
+                      List.iter (fun j -> Printf.printf " inner loop\n";
+                                          let id, name = j in
+                                          Printf.printf "  oid %s val %s\n"
+                                                        (String.concat "." (List.map string_of_int (OID.to_list id)))
+                                                        name)
+                                i)
+            cert.subject;
+  (match cert.extensions with
+   | Some x ->
+      List.iter (fun i ->
+                   Printf.printf "EXTENSION\n";
+                   let id, x, r = i in
+                   Printf.printf "  oid %s x %s"
+                                 (String.concat "." (List.map string_of_int (OID.to_list id)))
+                                 (string_of_bool x);
+                   Cstruct.hexdump r)
+                x;
+   | None -> Printf.printf "no extensions\n");
+  (match cert.issuer_id with
+   | Some x -> Printf.printf "issuer id"; Cstruct.hexdump x
+   | None -> Printf.printf "no issuer id\n");
+  (match cert.subject_id with
+   | Some x -> Printf.printf "subject id"; Cstruct.hexdump x
+   | None -> Printf.printf "no subject id\n");
   validate_time now cert
 
-let verify_certificate : certificate list -> float -> string -> certificate -> Cstruct.t -> verification_result =
+let verify_certificate : certificate -> float -> string -> certificate -> Cstruct.t -> verification_result =
   fun trusted now servername c raw ->
+  Printf.printf "verify certificate\n";
 (*
  - good for signing
  - all should have CA = true in Basic Constraints
@@ -96,7 +134,7 @@ let find_trusted_certs : unit -> certificate list =
 (*    let ca = Crypto_utils.cert_of_file "../mirage-server/server.pem" in *)
     [ca]
 
-let verify_server_certificate : certificate list -> float -> string -> certificate -> Cstruct.t -> verification_result =
+let verify_server_certificate : certificate -> float -> string -> certificate -> Cstruct.t -> verification_result =
   fun trusted now servername c raw ->
 (*
  - key usage basic constraint: certificate should be good for
@@ -110,6 +148,7 @@ let verify_server_certificate : certificate list -> float -> string -> certifica
  - the emailAddress in subject should match expected values ??
  *)
   (* first things first: valid signature, unwarp tbscert: validity timestamps,... *)
+  Printf.printf "verify server certificate\n";
   if validate_signature trusted c raw then
     let cert = c.tbs_cert in
     if basic_verification now servername cert then
@@ -119,33 +158,54 @@ let verify_server_certificate : certificate list -> float -> string -> certifica
   else
     `Fail InvalidSignature
 
+let verify_top_certificate : certificate list -> float -> string -> certificate -> Cstruct.t -> verification_result =
+  fun trusted now servername c raw ->
+  (* first have to find issuer of ``c`` in ``trusted`` *)
+    Printf.printf "verify top certificate\n";
+    let issuer = List.hd trusted in
+    if validate_signature issuer c raw then
+      let cert = c.tbs_cert in
+      if basic_verification now servername cert then
+        `Ok
+      else
+        `Fail InvalidCertificate
+    else
+      `Fail InvalidSignature
+
+
 (* this is the API for a user (Cstruct.t list will go away) *)
 let verify_certificates : string -> certificate list -> Cstruct.t list -> verification_result =
   fun servername cs packets ->
-    (* in reality we get a certificate chain, and the first is the server *)
+    (* we get a certificate chain, and the first is the server *)
     (* thus we need to reverse the list
         - check that the first is signed by some CA we know and trust
-        - check next to be signed by previous (look into issuer)
 
+        - check next to be signed by previous (look into issuer)
         - certificate verification lists...
     *)
     let now = Sys.time () in
-    let rec go trusted = function
-      | []    -> verify_server_certificate trusted now servername (List.hd cs) (List.hd packets)
-      | (c, p)::cs ->
-         match verify_certificate trusted now servername c p with
-         | `Ok  -> go [c] cs
-         | `Fail x -> `Fail x
+    let rec go t = function
+      | []    -> (`Ok, t)
+      | (c, p)::cs -> (* check that x is signed by x - 1 *)
+         match verify_certificate t now servername c p with
+         | `Ok  -> go c cs
+         | `Fail x -> (`Fail x, c)
     in
     let trusted = find_trusted_certs () in
-    go trusted (List.combine (List.rev (List.tl cs)) (List.rev (List.tl packets)))
+    let reversed = List.combine (List.rev (List.tl cs)) (List.rev (List.tl packets)) in
+    let topc, topr = List.hd reversed in
+    (* check that top one is signed by a trust anchor *)
+    match verify_top_certificate trusted now servername topc topr with
+    | `Ok -> (match go topc (List.tl reversed) (* checking rest of chain *) with
+              | (`Ok, t) -> verify_server_certificate t now servername (List.hd cs) (List.hd packets) (* check server certificate *)
+              | (`Fail x, _) -> `Fail x)
+    | `Fail x -> `Fail x
 
 (* - test setup (ACM CCS'12):
             self-signed cert with requested commonName,
             self-signed cert with other commonName,
             valid signed cert with other commonName
-   - also of interest: international domain names, wildcards
- *)
+   - also of interest: international domain names, wildcards *)
 
 (* alternative approach: interface and implementation for certificate pinning *)
 (* alternative approach': notary system / perspectives *)
