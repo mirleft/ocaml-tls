27b2b526a8f892c09d7f4685fabcf0d3abd1bcff
Hannes Mehnert <hannes@mehnert.org>
2014-03-07 23:58:12 +0000

    rewrite toplevel

--- lib/certificate.ml
+++ lib/certificate.ml
@@ -228,59 +228,47 @@ let find_issuer trusted cert =
   | [t] -> Some t
   | _   -> Printf.printf "found multiple root CAs\n"; None
 
-(* this is the API for a user (Cstruct.t list might go away) *)
-let verify_certificates ?servername : (certificate * Cstruct.t) list -> verification_result
-= function
+let rec verify_certificates_internal ?servername now trustanchor
+ = function
     (* we get the certificate chain cs:
-        [c0; c1; c2; ... ; cn]
-        let server = c0
-        let top = cn
+        [cn; cn-1; ... ; c2; c1; c0]
        strategy:
-        1. find a trusted CA for top, use it cn+1
-        2. verify intermediate certificates:
+        1. verify intermediate certificates:
              verify that [cn+1 .. c2] signed [cn .. c1]
-        3. verify server certificate was signed by c1 and
+        2. verify server certificate was signed by c1 and
              server certificate has required servername *)
-  | [] -> `Fail InvalidInput
+  | []                               -> `Fail InvalidInput
+  | [(server_cert, server_raw)] ->
+     (* step 2 *)
+     verify_server_certificate ?servername trustanchor now server_cert server_raw
+  | (cert, raw) :: cs                ->
+     (* step 1 *)
+     match verify_certificate trustanchor now cert raw with
+     | `Ok     -> verify_certificates_internal ?servername now cert cs
+     | `Fail x -> `Fail x
 
-    (* short-path for self-signed certificate  *)
-  | [(cert, _)] when is_self_signed cert ->
-      (* further verification of a self-signed certificate does not make sense:
+
+(* this is the API for a user (Cstruct.t list might go away) *)
+let verify_certificates ?servername certificates : verification_result =
+  if List.length certificates == 0 then
+    `Fail InvalidInput
+  else
+    let now = Sys.time () in
+    let trusted = find_trusted_certs now in
+    let reversed = List.rev certificates in
+    let top, _ = List.hd reversed in
+    match find_issuer trusted top with
+    | None ->
+       if is_self_signed top  then
+         (* further verification of a self-signed certificate does not make sense:
          why should anyone handle a properly self-signed and valid certificate
          different from a badly signed invalid certificate? *)
-      (Printf.printf "DANGER: self-signed certificate\n";
-       `Fail SelfSigned)
-
-  | (server, server_raw) :: certs_and_raw ->
-      let now = Sys.time () in
-      (* :( this is soooo foldr in a lazy setting... *)
-      let rec go trustanchor = function
-        | []              -> (`Ok, trustanchor)
-        | (cert, raw)::cs ->
-           match verify_certificate trustanchor now cert raw with
-           | `Ok     -> go cert cs
-           | `Fail x -> (`Fail x, cert)
-      in
-      let trusted = find_trusted_certs now in
-      (* intermediate certificates *)
-      match List.rev certs_and_raw with
-      | (topc, topr) :: reversed as certificate_chain ->
-         (* step 1 *)
-         (match find_issuer trusted topc with
-          | None -> `Fail NoTrustAnchor
-          | Some trustanchor ->
-             (* step 2 *)
-             (match go trustanchor certificate_chain with
-              | (`Ok, trustanchor) ->
-                 (* step 3 *)
-                 verify_server_certificate ?servername trustanchor now server server_raw
-              | (`Fail x, _) -> `Fail x))
-      | [] -> (* cert might be a direct sibling of the CA *)
-         match find_issuer trusted server with
-         | None -> `Fail NoTrustAnchor
-         | Some trustanchor ->
-            (* step 3 *)
-            verify_server_certificate ?servername trustanchor now server server_raw
+         (Printf.printf "DANGER: self-signed certificate\n";
+          `Fail SelfSigned)
+       else
+         `Fail NoTrustAnchor
+    | Some trustanchor ->
+       verify_certificates_internal ?servername now trustanchor reversed
 
 
 (* TODO: how to deal with
