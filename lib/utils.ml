704561a43b0cb30d150e42476fc14be96b6199f5
pqwy <unitedbiscuits@gmail.com>
2014-03-07 19:12:53 +0100

    control-flow combinators, vol. 17

--- lib/utils.ml
+++ lib/utils.ml
@@ -43,3 +43,17 @@ let cs_eq cs1 cs2 =
     cmp cs1 cs2 len
   else
     false
+
+let rec filter_map ~f = function
+  | []    -> []
+  | x::xs ->
+      match f x with
+      | None    -> filter_map ~f xs
+      | Some x' -> x' :: filter_map ~f xs
+
+let rec map_find ~f = function
+  | []    -> None
+  | x::xs ->
+      match f x with
+      | None         -> map_find ~f xs
+      | Some _ as x' -> x'
