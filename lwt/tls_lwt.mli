96a313596aba7d3cb7858379a50ffe7d7b2559c2
pqwy <david@numm.org>
2014-04-26 06:18:27 +0200

    expand the lwt api

--- lwt/tls_lwt.mli
+++ lwt/tls_lwt.mli
@@ -9,9 +9,8 @@ val read     : socket -> Cstruct.t Lwt.t
 val write    : socket -> Cstruct.t -> unit Lwt.t
 val writev   : socket -> Cstruct.t list -> unit Lwt.t
 
-val server_of_fd : Lwt_unix.file_descr -> socket Lwt.t
-val client_of_fd : ?host:string -> Lwt_unix.file_descr -> socket Lwt.t
+val server_of_fd : ?cert:X509_lwt.cert -> Lwt_unix.file_descr -> socket Lwt.t
+val client_of_fd : ?cert:X509_lwt.cert -> ?host:string -> Lwt_unix.file_descr -> socket Lwt.t
 
-val accept  : Lwt_unix.file_descr -> (socket * Lwt_unix.sockaddr) Lwt.t
-val connect : ?fd:Lwt_unix.file_descr -> host:string -> port:string -> socket Lwt.t
-
+val accept  : ?cert:X509_lwt.cert -> Lwt_unix.file_descr -> (socket * Lwt_unix.sockaddr) Lwt.t
+val connect : ?fd:Lwt_unix.file_descr -> ?cert:X509_lwt.cert -> host:string -> port:string -> socket Lwt.t
