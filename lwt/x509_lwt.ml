1253935e24a95b910cc2676e400da21fc18b55ea
pqwy <david@numm.org>
2014-04-26 09:23:12 +0200

    handy loaders

--- lwt/x509_lwt.ml
+++ lwt/x509_lwt.ml
@@ -9,6 +9,29 @@ let read_file path =
   lwt str  = read file in
   close file >> return (Cstruct.of_string str)
 
+let read_dir path =
+  let open Lwt_unix in
+  let rec collect acc d =
+    match_lwt
+      try_lwt readdir d >|= fun e -> Some e with End_of_file -> return None
+    with
+    | Some e -> collect (e :: acc) d
+    | None   -> return acc in
+  lwt dir     = opendir path in
+  lwt entries = collect [] dir in
+  closedir dir >> return entries
+
+let extension str =
+  let n = String.length str in
+  let rec scan = function
+    | i when i = 0         -> None
+    | i when str.[i - 1] = '.' ->
+        Some (String.sub str i (n - i))
+    | i                    -> scan (pred i) in
+  scan n
+
+let (</>) a b = a ^ "/" ^ b
+
 let cert_of_pems ~cert ~priv_key =
   lwt cs_cert = read_file cert
   and cs_pk   = read_file priv_key in
@@ -21,4 +44,26 @@ let cert_of_pems ~cert ~priv_key =
     with Invalid_argument msg -> fail (Failure ("key: " ^ msg))
   in
   return (cert, pk)
+
+let certs_of_pem path =
+  lwt cs_certs = read_file path in
+  try
+    return @@ Tls.X509.Cert.of_pem_cstruct cs_certs
+  with Invalid_argument msg -> fail (Failure ("certificates: " ^ msg))
+
+let certs_of_pem_dir path =
+  lwt files = read_dir path in
+  files
+  |> List.filter (fun file -> extension file = Some "crt")
+  |> Lwt_list.map_p (fun file -> certs_of_pem (path </> file))
+  >|= List.concat
+
+let validator = function
+  | `Ca_file path ->
+      certs_of_pem path >|= fun cas ->
+        Tls.X509.Validator.chain_of_trust ~time:0 cas
+  | `Ca_dir path ->
+      certs_of_pem_dir path >|= fun cas ->
+        Tls.X509.Validator.chain_of_trust ~time:0 cas
+  | `No_validation -> return Tls.X509.Validator.null
 
--- lwt/x509_lwt.mli
+++ lwt/x509_lwt.mli
@@ -3,3 +3,12 @@ type cert = Tls.X509.Cert.t * Tls.X509.PK.t
 
 val cert_of_pems : cert:Lwt_io.file_name -> priv_key:Lwt_io.file_name -> cert Lwt.t
 
+val certs_of_pem     : Lwt_io.file_name -> Tls.X509.Cert.t list Lwt.t
+val certs_of_pem_dir : Lwt_io.file_name -> Tls.X509.Cert.t list Lwt.t
+
+val validator :
+  [ `Ca_file of Lwt_io.file_name
+  | `Ca_dir  of Lwt_io.file_name
+  | `No_validation ] ->
+  Tls.X509.Validator.t Lwt.t
+
