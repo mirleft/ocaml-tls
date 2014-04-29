cd3662d9ba8651a235893ae4335a04a66e854142
Hannes Mehnert <hannes@mehnert.org>
2014-04-29 14:38:16 +0200

    first test

--- unittests/unittests.ml
+++ unittests/unittests.ml
@@ -1,6 +1,19 @@
 open OUnit2
+open Cstruct
+open Tls
+
+let parse_version_0 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 3;
+  set_uint8 ver 1 1;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_0
+  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
 
 let suite =
   "All" >::: [
-    "BLA" >::: []
+    "Reader" >::: [
+      "Parse_version_int" >:: parse_version_0
+    ]
   ]
+
