44c8f725e2d13cd6406d15961e8e32a4f1d00c2f
Hannes Mehnert <hannes@mehnert.org>
2014-04-29 14:58:13 +0200

    more tests for version parsing

--- unittests/unittests.ml
+++ unittests/unittests.ml
@@ -5,15 +5,86 @@ open Tls
 let parse_version_0 _ =
   let ver = create 2 in
   set_uint8 ver 0 3;
+  set_uint8 ver 1 0;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_equal v Core.SSL_3
+  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+
+let parse_version_1 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 3;
   set_uint8 ver 1 1;
   match Reader.parse_version ver with
   | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_0
   | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
 
+let parse_version_2 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 3;
+  set_uint8 ver 1 2;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_1
+  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+
+let parse_version_3 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 3;
+  set_uint8 ver 1 3;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_2
+  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+
+let parse_version_4 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 3;
+  set_uint8 ver 1 4;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_X
+  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+
+let parse_version_5 _ =
+  let ver = create 1 in
+  set_uint8 ver 0 3;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
+  | Reader.Or_error.Error _ -> assert_bool "length too short" true
+
+let parse_version_6 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 2;
+  set_uint8 ver 1 4;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
+  | Reader.Or_error.Error _ -> assert_bool "unknown version" true
+
+let parse_version_7 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 3;
+  set_uint8 ver 1 42;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_equal v TLS_1_X
+  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+
+let parse_version_8 _ =
+  let ver = create 2 in
+  set_uint8 ver 0 4;
+  set_uint8 ver 1 4;
+  match Reader.parse_version ver with
+  | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
+  | Reader.Or_error.Error _ -> assert_bool "unknown version" true
+
 let suite =
   "All" >::: [
     "Reader" >::: [
-      "Parse_version_int" >:: parse_version_0
+      "Parse_version 0" >:: parse_version_0 ;
+      "Parse_version 1" >:: parse_version_1 ;
+      "Parse_version 2" >:: parse_version_2 ;
+      "Parse_version 3" >:: parse_version_3 ;
+      "Parse_version 4" >:: parse_version_4 ;
+      "Parse_version 5" >:: parse_version_5 ;
+      "Parse_version 6" >:: parse_version_6 ;
+      "Parse_version 7" >:: parse_version_7 ;
+      "Parse_version 8" >:: parse_version_8 ;
     ]
   ]
 
