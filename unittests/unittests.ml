5c2349fc0fc069838929ee12b5df55d88ff837a2
Hannes Mehnert <hannes@mehnert.org>
2014-04-29 15:31:10 +0200

    style

--- unittests/unittests.ml
+++ unittests/unittests.ml
@@ -1,87 +1,44 @@
 open OUnit2
-open Cstruct
 open Tls
 
-let parse_version_0 _ =
+let good_version_parser major minor result _ =
+  let open Cstruct in
   let ver = create 2 in
-  set_uint8 ver 0 3;
-  set_uint8 ver 1 0;
+  set_uint8 ver 0 major;
+  set_uint8 ver 1 minor;
   match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_equal v Core.SSL_3
+  | Reader.Or_error.Ok v    -> assert_equal v result
   | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
 
-let parse_version_1 _ =
+let bad_version_parser major minor _ =
+  let open Cstruct in
   let ver = create 2 in
-  set_uint8 ver 0 3;
-  set_uint8 ver 1 1;
+  set_uint8 ver 0 major;
+  set_uint8 ver 1 minor;
   match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_0
-  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+  | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
+  | Reader.Or_error.Error _ -> assert_bool "unknown version" true
 
-let parse_version_2 _ =
-  let ver = create 2 in
-  set_uint8 ver 0 3;
-  set_uint8 ver 1 2;
-  match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_1
-  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
-
-let parse_version_3 _ =
-  let ver = create 2 in
-  set_uint8 ver 0 3;
-  set_uint8 ver 1 3;
-  match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_2
-  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
-
-let parse_version_4 _ =
-  let ver = create 2 in
-  set_uint8 ver 0 3;
-  set_uint8 ver 1 4;
-  match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_equal v Core.TLS_1_X
-  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
-
-let parse_version_5 _ =
+let parse_version_too_short _ =
+  let open Cstruct in
   let ver = create 1 in
   set_uint8 ver 0 3;
   match Reader.parse_version ver with
   | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
   | Reader.Or_error.Error _ -> assert_bool "length too short" true
 
-let parse_version_6 _ =
-  let ver = create 2 in
-  set_uint8 ver 0 2;
-  set_uint8 ver 1 4;
-  match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
-  | Reader.Or_error.Error _ -> assert_bool "unknown version" true
+let version_parser_tests = [ good_version_parser 3 0 Core.SSL_3 ;
+                             good_version_parser 3 1 Core.TLS_1_0 ;
+                             good_version_parser 3 2 Core.TLS_1_1 ;
+                             good_version_parser 3 3 Core.TLS_1_2 ;
+                             good_version_parser 3 4 Core.TLS_1_X ;
+                             good_version_parser 3 42 Core.TLS_1_X ;
 
-let parse_version_7 _ =
-  let ver = create 2 in
-  set_uint8 ver 0 3;
-  set_uint8 ver 1 42;
-  match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_equal v TLS_1_X
-  | Reader.Or_error.Error _ -> assert_failure "Version parser broken"
+                             bad_version_parser 2 4 ;
+                             bad_version_parser 4 4 ;
+                             bad_version_parser 0 2 ;
 
-let parse_version_8 _ =
-  let ver = create 2 in
-  set_uint8 ver 0 4;
-  set_uint8 ver 1 4;
-  match Reader.parse_version ver with
-  | Reader.Or_error.Ok v    -> assert_failure "Version parser broken"
-  | Reader.Or_error.Error _ -> assert_bool "unknown version" true
-
-let version_parser_tests = [ parse_version_0 ;
-                             parse_version_1 ;
-                             parse_version_2 ;
-                             parse_version_3 ;
-                             parse_version_4 ;
-                             parse_version_5 ;
-                             parse_version_6 ;
-                             parse_version_7 ;
-                             parse_version_8 ]
+                             parse_version_too_short ]
 
 let suite =
   "All" >::: [
