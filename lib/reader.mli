8e77f21946d2070d40628f28922390b320f7bb0c
pqwy <david@numm.org>
2014-05-04 23:20:58 +0200

    expose all the errors from reader

--- lib/reader.mli
+++ lib/reader.mli
@@ -1,17 +1,21 @@
 
 type error =
-  | Overflow
+  | TrailingBytes of string
+  | WrongLength   of string
+  | Unknown       of string
+  | Underflow
 
 module Or_error :
   Control.Or_error with type err = error
+open Or_error
 
-val parse_version   : Cstruct.t -> Core.tls_version Or_error.or_error
+val parse_version   : Cstruct.t -> Core.tls_version or_error
 val parse_hdr       : Cstruct.t -> Packet.content_type option * Core.tls_version option * int
 
-val parse_handshake : Cstruct.t -> Core.tls_handshake Or_error.or_error
+val parse_handshake : Cstruct.t -> Core.tls_handshake or_error
 
-val parse_alert     : Cstruct.t -> Core.tls_alert Or_error.or_error
+val parse_alert     : Cstruct.t -> Core.tls_alert or_error
 
-val parse_dh_parameters : Cstruct.t -> (Core.dh_parameters * Cstruct.t * Cstruct.t) Or_error.or_error
-val parse_digitally_signed : Cstruct.t -> Cstruct.t Or_error.or_error
-val parse_digitally_signed_1_2 : Cstruct.t -> (Ciphersuite.hash_algorithm * Packet.signature_algorithm_type * Cstruct.t) Or_error.or_error
+val parse_dh_parameters : Cstruct.t -> (Core.dh_parameters * Cstruct.t * Cstruct.t) or_error
+val parse_digitally_signed : Cstruct.t -> Cstruct.t or_error
+val parse_digitally_signed_1_2 : Cstruct.t -> (Ciphersuite.hash_algorithm * Packet.signature_algorithm_type * Cstruct.t) or_error
