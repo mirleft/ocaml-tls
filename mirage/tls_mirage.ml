e9ef860dbc60f562aa2ca574a18e6c3abcbd0036
pqwy <david@numm.org>
2014-05-07 06:01:11 +0200

    core read+write loop

--- mirage/tls_mirage.ml
+++ mirage/tls_mirage.ml
@@ -23,5 +23,35 @@ module TLS ( TCP : TCPV4' ) = struct
     mutable linger : Cstruct.t list ;
   }
 
+  open Lwt
+
+  let read_react flow =
+    match flow.state with
+    | ( `Eof | `Error _ ) as e -> return e
+    | `Active state ->
+        TCP.read flow.tcp >>= function
+          | ( `Eof | `Error _ ) as e ->
+              flow.state <- e ; return e
+          | `Ok buf ->
+              match
+                ( match flow.role with
+                  | `Server -> Tls.Server.handle_tls
+                  | `Client -> Tls.Client.handle_tls )
+                state buf
+              with
+              | `Ok (state, answer, appdata) ->
+                  flow.state <- `Active state ;
+                  TCP.write flow.tcp answer >> return (`Ok appdata)
+              | `Fail (alert, answer) ->
+                  let reason =
+                    match alert with
+                    | Tls.Packet.CLOSE_NOTIFY -> `Eof
+                    | _ ->
+                        let repr = Tls.Packet.alert_type_to_string alert in
+                        `Error (`Unknown repr) in
+                  flow.state <- reason ;
+                  TCP.( write flow.tcp answer >> close flow.tcp )
+                  >> return reason
+
 
 end
