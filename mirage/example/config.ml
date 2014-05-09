1f4460a8864b32ad090f6463824c2eb9e52f90ef
pqwy <david@numm.org>
2014-05-09 06:20:02 +0200

    pay lip service to xen builds
    and watch it break

--- mirage/example/config.ml
+++ mirage/example/config.ml
@@ -7,6 +7,11 @@ let disk =
   | `Unix -> direct_kv_ro secrets_dir
   | `Xen  -> crunch secrets_dir
 
+let stack =
+  match get_mode () with
+  | `Unix -> socket_stackv4 default_console [Ipaddr.V4.any]
+  | `Xen  -> direct_stackv4_with_default_ipv4 default_console tap0
+
 let server =
   foreign "Unikernel.Server" @@ console @-> stackv4 @-> kv_ro @-> job
 
@@ -14,7 +19,7 @@ let () =
   add_to_ocamlfind_libraries ["tls"; "tls.mirage"] ;
   register "tls-server" [
     server $ default_console
-           $ socket_stackv4 default_console [Ipaddr.V4.any]
+           $ stack
            $ disk
   ]
 
