4743b203bc3fe2bee9c8efb3bf2226bfd8662778
Hannes Mehnert <hannes@mehnert.org>
2014-05-11 15:48:50 +0200

    use the very same API for echo_client as for http_client:  by default, use the certificate bundle from our repository  provide an undocumented command line option to disable certificate checking

--- lwt/examples/echo_client.ml
+++ lwt/examples/echo_client.ml
@@ -2,8 +2,13 @@
 open Ex_common
 open Lwt
 
-let echo_client host port =
-  lwt validator = X509_lwt.validator (`No_validation_I'M_STUPID) in
+let echo_client ?ca host port =
+  lwt validator = X509_lwt.validator
+    (match ca with
+     | None        -> `Ca_dir ca_cert_dir
+     | Some "NONE" -> `No_validation_I'M_STUPID
+     | Some f      -> `Ca_file f)
+  in
   lwt sock      = Tls_lwt.connect validator host port
   in
   let rec network () =
@@ -14,8 +19,13 @@ let echo_client host port =
   Lwt.join [ network () ; keyboard () ]
 
 let () =
-  match Sys.argv with
-  | [| _ ; host ; port |] -> Lwt_main.run (echo_client host port)
-  | [| _ ; host |]        -> Lwt_main.run (echo_client host "443")
-  | args                  -> Printf.eprintf "%s <host> <port>\n%!" args.(0)
+  try (
+    match Sys.argv with
+    | [| _ ; host ; port ; trust |] -> Lwt_main.run (echo_client host port ~ca:trust)
+    | [| _ ; host ; port |]         -> Lwt_main.run (echo_client host port)
+    | [| _ ; host |]                -> Lwt_main.run (echo_client host "443")
+    | args                          -> Printf.eprintf "%s <host> <port>\n%!" args.(0) ) with
+  | Tls_lwt.Tls_alert al ->
+     Printf.eprintf "TLS ALERT: %s\n%!" (Tls.Packet.alert_type_to_string al) ;
+     raise (Tls_lwt.Tls_alert al)
 
