7fdf862b0c564a3f82a39bb2271c5ab19bfec6b3
pqwy <david@numm.org>
2014-05-28 02:52:47 +0100

    widen the interface of tlsv4 mock-module

--- mirage/tls_mirage.mli
+++ mirage/tls_mirage.mli
@@ -3,7 +3,12 @@ module Make (TCP : V1_LWT.TCPV4) : sig
   include Tls_mirage_types.TLS_core
 end with module TCP := TCP
 
-module Make_flow (TCP : V1_LWT.TCPV4) : V1_LWT.TCPV4
+module Make_flow (TCP : V1_LWT.TCPV4) : sig
+  include Tls_mirage_types.TLS_core
+  include V1_LWT.TCPV4
+    with type flow  := flow
+     and type error := error
+end with module TCP := TCP
 
 (* XXX CLOCK *)
 module X509 (KV : V1_LWT.KV_RO) : sig
