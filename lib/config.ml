8a28ef2adee11c64c1fef245bc0867b35204c9b2
pqwy <david@numm.org>
2014-06-03 15:49:51 +0100

    start encapsulating config

--- lib/config.ml
+++ lib/config.ml
@@ -1,8 +1,9 @@
 open Core
 
+exception Invalid_configuration of string
+
 type own_cert = Certificate.certificate * Nocrypto.RSA.priv
 
-(* some config parameters *)
 type config = {
   ciphers                 : Ciphersuite.ciphersuite list ;
   protocol_versions       : tls_version * tls_version ;
@@ -15,26 +16,70 @@ type config = {
   own_certificate         : own_cert option ;
 }
 
-let default_config = {
-  (* ordered list (regarding preference) of supported cipher suites *)
-  ciphers = Ciphersuite.([ TLS_RSA_WITH_AES_256_CBC_SHA ;
-                           TLS_DHE_RSA_WITH_AES_256_CBC_SHA ;
-                           TLS_RSA_WITH_AES_128_CBC_SHA ;
-                           TLS_DHE_RSA_WITH_AES_128_CBC_SHA ;
-                           TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA ;
-                           TLS_RSA_WITH_3DES_EDE_CBC_SHA ;
-                           TLS_RSA_WITH_RC4_128_SHA ;
-                           TLS_RSA_WITH_RC4_128_MD5 ]) ;
-  (* ordered list of decreasing protocol versions *)
-  protocol_versions = ( TLS_1_2 , TLS_1_0 ) ;
-  (* ordered list (regarding preference) *)
-  hashes = Ciphersuite.([ SHA512 ; SHA384 ; SHA256 ; SHA ; MD5 ]) ;
-  (* signatures = [ Packet.RSA ] *)
-  (* whether or not to rekey *)
-  use_rekeying = true ;
-  require_secure_rekeying = true ;
-  validator = None ;
-  peer_name = None ;
-  own_certificate = None ;
-}
+let supported_hashes =
+  Ciphersuite.([ SHA512 ; SHA384 ; SHA256 ; SHA ; MD5 ])
 
+let supported_ciphers = Ciphersuite.([
+    TLS_RSA_WITH_AES_256_CBC_SHA ;
+    TLS_DHE_RSA_WITH_AES_256_CBC_SHA ;
+    TLS_RSA_WITH_AES_128_CBC_SHA ;
+    TLS_DHE_RSA_WITH_AES_128_CBC_SHA ;
+    TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA ;
+    TLS_RSA_WITH_3DES_EDE_CBC_SHA ;
+    TLS_RSA_WITH_RC4_128_SHA ;
+    TLS_RSA_WITH_RC4_128_MD5
+  ])
+
+let invalid msg = raise (Invalid_configuration msg)
+
+let validate_exn config =
+  let (v_max, v_min) = config.protocol_versions in
+  if v_max < v_min then invalid "bad version range" ;
+  if config.require_secure_rekeying &&
+     not config.use_rekeying
+  then invalid "rekeying disabled but secure rekeying required" ;
+  ( match config.hashes with
+    | [] when v_max >= TLS_1_2 ->
+        invalid "TLS 1.2 allowed but no hashes specified"
+    | _ -> () )
+
+
+let create ?( ciphers  = supported_ciphers )
+           ?( version  = (TLS_1_2, TLS_1_0) )
+           ?( hashes   = supported_hashes )
+           ?( rekeying  = true )
+           ?( secure_rekeying_required = true )
+           ?validator
+           ?peer_name
+           ?certificate
+           () =
+  let config = {
+        ciphers                 = ciphers ;
+        protocol_versions       = version ;
+        hashes                  = hashes ;
+        use_rekeying            = rekeying ;
+        require_secure_rekeying = secure_rekeying_required ;
+        validator               = validator ;
+        peer_name               = peer_name ;
+        own_certificate         = certificate ;
+    } in
+  ( validate_exn config ; config )
+
+(* |+ client +|
+let open_connection ?cert ?host:server ~validator () =
+  let open Config in
+  let config =
+  {
+    default_config with
+      validator = Some validator ;
+      own_certificate = cert ;
+      peer_name = server
+  }
+  in
+  open_connection' config
+
+|+ server +|
+let listen_connection ?cert () =
+  let open Config in
+  let conf = { default_config with own_certificate = cert } in
+  new_state conf `Server *)
--- lib/config.mli
+++ lib/config.mli
@@ -1,11 +1,16 @@
 open Core
 
+exception Invalid_configuration of string
+
 type own_cert = Certificate.certificate * Nocrypto.RSA.priv
 
 (* some config parameters *)
-type config = {
+type config = private {
+  (* ordered list (regarding preference) of supported cipher suites *)
   ciphers                 : Ciphersuite.ciphersuite list ;
+  (* (max, min) *)
   protocol_versions       : tls_version * tls_version ;
+  (* ordered list (regarding preference) *)
   hashes                  : Ciphersuite.hash_algorithm list ;
   (* signatures              : Packet.signature_algorithm_type list ; *)
   use_rekeying            : bool ;
@@ -15,4 +20,14 @@ type config = {
   own_certificate         : own_cert option ;
 }
 
-val default_config : config
+val supported_ciphers : Ciphersuite.ciphersuite list
+val supported_hashes  : Ciphersuite.hash_algorithm list
+val create : ?ciphers:     Ciphersuite.ciphersuite list    ->
+             ?version:     tls_version * tls_version       ->
+             ?hashes:      Ciphersuite.hash_algorithm list ->
+             ?rekeying:    bool                            ->
+             ?secure_rekeying_required: bool               ->
+             ?validator:   X509.Validator.t                ->
+             ?peer_name:   string                          ->
+             ?certificate: own_cert                        ->
+             unit -> config
