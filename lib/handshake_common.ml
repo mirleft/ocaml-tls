cdcf915a5152c8e44b4e01ae83d2b87afc58b146
Hannes Mehnert <hannes@mehnert.org>
2014-06-03 20:04:34 +0100

    style

--- lib/handshake_common.ml
+++ lib/handshake_common.ml
@@ -22,9 +22,9 @@ let get_secure_renegotiation exts =
 
 let supported_protocol_version (max, min) v =
   match v >= max, v >= min with
-    | true, _ -> Some max
-    | _ , true -> Some v
-    | _ , _ -> None
+    | true, _    -> Some max
+    | _   , true -> Some v
+    | _   , _    -> None
 
 let rec not_multiple_same_extensions = function
   | (Hostname _)::(Hostname _)::xs -> invalid_arg "multiple hostname extensions"
