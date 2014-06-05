f1ac23bb0ab5f419a19fb49c7fb5b447f137d907
David Kaloper <david@numm.org>
2014-06-05 01:56:35 +0100

    port lwt

--- lwt/tls_lwt.mli
+++ lwt/tls_lwt.mli
@@ -2,20 +2,17 @@
 exception Tls_alert   of Tls.Packet.alert_type
 exception Tls_failure of Tls.Packet.alert_type
 
-type o_server = X509_lwt.priv
-type o_client = X509_lwt.validator
-
 module Unix : sig
 
   type t
 
   val close : t -> unit Lwt.t
 
-  val server_of_fd : o_server ->                Lwt_unix.file_descr -> t Lwt.t
-  val client_of_fd : o_client -> host:string -> Lwt_unix.file_descr -> t Lwt.t
+  val server_of_fd : Tls.Config.server -> Lwt_unix.file_descr -> t Lwt.t
+  val client_of_fd : Tls.Config.client -> host:string -> Lwt_unix.file_descr -> t Lwt.t
 
-  val accept  : o_server -> Lwt_unix.file_descr -> (t * Lwt_unix.sockaddr) Lwt.t
-  val connect : o_client -> string * int -> t Lwt.t
+  val accept  : Tls.Config.server -> Lwt_unix.file_descr -> (t * Lwt_unix.sockaddr) Lwt.t
+  val connect : Tls.Config.client -> string * int -> t Lwt.t
 
   val read   : t -> Cstruct.t      -> int  Lwt.t
   val write  : t -> Cstruct.t      -> unit Lwt.t
@@ -29,8 +26,14 @@ end
 type ic = Lwt_io.input_channel
 type oc = Lwt_io.output_channel
 
-val accept : o_server -> Lwt_unix.file_descr
-                      -> ((ic * oc) * Lwt_unix.sockaddr) Lwt.t
-val connect : o_client -> string * int -> (ic * oc) Lwt.t
+val accept_ext : Tls.Config.server -> Lwt_unix.file_descr ->
+                 ((ic * oc) * Lwt_unix.sockaddr) Lwt.t
+
+val accept : X509_lwt.priv -> Lwt_unix.file_descr ->
+             ((ic * oc) * Lwt_unix.sockaddr) Lwt.t
+
+val connect_ext : Tls.Config.client -> string * int -> (ic * oc) Lwt.t
+
+val connect : X509_lwt.validator -> string * int -> (ic * oc) Lwt.t
 
 val of_t : Unix.t -> ic * oc
