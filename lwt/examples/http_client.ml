3c2dbbd8200e62e18f19a9f78a3592ec516cee32
David Kaloper <david@numm.org>
2014-06-05 02:02:55 +0100

    http_client: don't require secure rekeying
    :)

--- lwt/examples/http_client.ml
+++ lwt/examples/http_client.ml
@@ -3,27 +3,34 @@ open Lwt
 open Ex_common
 
 let http_client ?ca host port =
-  let port = int_of_string port in
+  let port      = int_of_string port in
   lwt validator = X509_lwt.validator
-    (match ca with
-     | None        -> `Ca_dir ca_cert_dir
-     | Some "NONE" -> `No_validation_I'M_STUPID
-     | Some f      -> `Ca_file f)
+    ( match ca with
+      | None        -> `Ca_dir ca_cert_dir
+      | Some "NONE" -> `No_validation_I'M_STUPID
+      | Some f      -> `Ca_file f )
   in
-  lwt (ic, oc) = Tls_lwt.connect validator (host, port) in
+  let config = Tls.Config.client ~validator ~require_secure_rekeying:false () in
+  lwt (ic, oc) = Tls_lwt.connect_ext config (host, port) in
   let req = String.concat "\r\n" [
     "GET / HTTP/1.1" ; "Host: " ^ host ; "Connection: close" ; "" ; ""
   ] in
   Lwt_io.(write oc req >> read ic >>= print >> printf "++ done.\n%!")
 
+let print_alert where alert =
+    Printf.eprintf "TLS ALERT (%s): %s\n%!"
+      where (Tls.Packet.alert_type_to_string alert)
+
 let () =
-  try (
+  try
     match Sys.argv with
     | [| _ ; host ; port ; trust |] -> Lwt_main.run (http_client host port ~ca:trust)
     | [| _ ; host ; port |]         -> Lwt_main.run (http_client host port)
     | [| _ ; host |]                -> Lwt_main.run (http_client host "443")
-    | args                          -> Printf.eprintf "%s <host> <port>\n%!" args.(0) ) with
-  | Tls_lwt.Tls_alert al ->
-     Printf.eprintf "TLS ALERT: %s\n%!" (Tls.Packet.alert_type_to_string al) ;
-     raise (Tls_lwt.Tls_alert al)
+    | args                          -> Printf.eprintf "%s <host> <port>\n%!" args.(0)
+  with
+  | Tls_lwt.Tls_alert alert as exn ->
+      print_alert "remote end" alert ; raise exn
+  | Tls_lwt.Tls_failure alert as exn ->
+      print_alert "our end" alert ; raise exn
 
