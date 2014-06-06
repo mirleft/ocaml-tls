4476ce6d61b893898a758c11e7ac46bfa7289267
David Kaloper <david@numm.org>
2014-06-06 16:31:37 +0100

    Cstruct_with_state -> Cstruct_s
    Soo much shorter..

--- lib/core.ml
+++ lib/core.ml
@@ -12,7 +12,7 @@ module Trace = struct
   let item id x = ( Tracing.item id x ; return () )
 end
 
-module Cstruct_with_sexp = struct
+module Cstruct_s = struct
   type t = Cstruct.t
   let t_of_sexp _ = assert false
   let sexp_of_t _ = assert false
@@ -54,16 +54,16 @@ type extension =
   | MaxFragmentLength of max_fragment_length
   | EllipticCurves of named_curve_type list
   | ECPointFormats of ec_point_format list
-  | SecureRenegotiation of Cstruct_with_sexp.t
+  | SecureRenegotiation of Cstruct_s.t
   | Padding of int
   | SignatureAlgorithms of (hash_algorithm * signature_algorithm_type) list
-  | UnknownExtension of (int * Cstruct_with_sexp.t)
+  | UnknownExtension of (int * Cstruct_s.t)
   with sexp
 
 type 'a hello = {
   version      : tls_version;
-  random       : Cstruct_with_sexp.t;
-  sessionid    : Cstruct_with_sexp.t option;
+  random       : Cstruct_s.t;
+  sessionid    : Cstruct_s.t option;
   ciphersuites : 'a;
   extensions   : extension list
 } with sexp
@@ -75,45 +75,45 @@ type server_hello = ciphersuite hello
   with sexp
 
 type rsa_parameters = {
-  rsa_modulus  : Cstruct_with_sexp.t;
-  rsa_exponent : Cstruct_with_sexp.t;
+  rsa_modulus  : Cstruct_s.t;
+  rsa_exponent : Cstruct_s.t;
 } with sexp
 
 type dh_parameters = {
-  dh_p  : Cstruct_with_sexp.t;
-  dh_g  : Cstruct_with_sexp.t;
-  dh_Ys : Cstruct_with_sexp.t;
+  dh_p  : Cstruct_s.t;
+  dh_g  : Cstruct_s.t;
+  dh_Ys : Cstruct_s.t;
 } with sexp
 
 type ec_curve = {
-  a : Cstruct_with_sexp.t;
-  b : Cstruct_with_sexp.t
+  a : Cstruct_s.t;
+  b : Cstruct_s.t
 } with sexp
 
 type ec_prime_parameters = {
-  prime    : Cstruct_with_sexp.t;
+  prime    : Cstruct_s.t;
   curve    : ec_curve;
-  base     : Cstruct_with_sexp.t;
-  order    : Cstruct_with_sexp.t;
-  cofactor : Cstruct_with_sexp.t;
-  public   : Cstruct_with_sexp.t
+  base     : Cstruct_s.t;
+  order    : Cstruct_s.t;
+  cofactor : Cstruct_s.t;
+  public   : Cstruct_s.t
 } with sexp
 
 type ec_char_parameters = {
   m        : int;
   basis    : ec_basis_type;
-  ks       : Cstruct_with_sexp.t list;
+  ks       : Cstruct_s.t list;
   curve    : ec_curve;
-  base     : Cstruct_with_sexp.t;
-  order    : Cstruct_with_sexp.t;
-  cofactor : Cstruct_with_sexp.t;
-  public   : Cstruct_with_sexp.t
+  base     : Cstruct_s.t;
+  order    : Cstruct_s.t;
+  cofactor : Cstruct_s.t;
+  public   : Cstruct_s.t
 } with sexp
 
 type ec_parameters =
   | ExplicitPrimeParameters of ec_prime_parameters
   | ExplicitCharParameters of ec_char_parameters
-  | NamedCurveParameters of (named_curve_type * Cstruct_with_sexp.t)
+  | NamedCurveParameters of (named_curve_type * Cstruct_s.t)
   with sexp
 
 type certificate_request = {
@@ -126,11 +126,11 @@ type tls_handshake =
   | ServerHelloDone
   | ClientHello of client_hello
   | ServerHello of server_hello
-  | Certificate of Cstruct_with_sexp.t list
-  | ServerKeyExchange of Cstruct_with_sexp.t
+  | Certificate of Cstruct_s.t list
+  | ServerKeyExchange of Cstruct_s.t
   | CertificateRequest of certificate_request
-  | ClientKeyExchange of Cstruct_with_sexp.t
-  | Finished of Cstruct_with_sexp.t
+  | ClientKeyExchange of Cstruct_s.t
+  | Finished of Cstruct_s.t
   with sexp
 
 type tls_alert = alert_level * alert_type
