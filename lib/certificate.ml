115a97ba647f7a7c7a5e668ab0882525b90f23d3
Hannes Mehnert <hannes@mehnert.org>
2014-06-07 20:34:27 +0100

    style: rename subject to subject_common_name

--- lib/certificate.ml
+++ lib/certificate.ml
@@ -131,12 +131,12 @@ let issuer_matches_subject { asn = parent } { asn = cert } =
 
 let is_self_signed cert = issuer_matches_subject cert cert
 
-let subject cert =
+let subject_common_name cert =
   map_find cert.tbs_cert.subject
            ~f:(function Name.CN n -> Some n | _ -> None)
 
 let common_name_to_string { asn = cert } =
-  match subject cert with
+  match subject_common_name cert with
   | None   -> "NO commonName:" ^ Utils.hexdump_to_str cert.signature_val
   | Some x -> x
 
