da55a86ccb49a3855f8a8dd5c8c36106345c5fd7
Hannes Mehnert <hannes@mehnert.org>
2014-07-26 19:08:00 +0100

    initialise_crypto_ctx is now epoch_data -> handshake_params -> premaster

--- lib/handshake_crypto.ml
+++ lib/handshake_crypto.ml
@@ -61,11 +61,13 @@ let divide_keyblock version key mac iv buf =
   (c_mac, s_mac, c_key, s_key, c_iv, s_iv)
 
 
-let initialise_crypto_ctx version params premaster =
+let initialise_crypto_ctx epoch params premaster =
   let open Ciphersuite in
   let client_random = params.client_random
   and server_random = params.server_random
-  and cipher = params.cipher in
+  and cipher = epoch.ciphersuite
+  and version = epoch.protocol_version
+  in
 
   let master = generate_master_secret version premaster
                 (client_random <+> server_random) in
