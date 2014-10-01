1f4d5696904db1adb19cc5d02d51c97c948748e8
Hannes Mehnert <hannes@mehnert.org>
2014-10-01 12:38:45 +0200

    add libgmp-dev to required packages

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,7 +9,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libgmp-dev
 
 export OPAMYES=1
 
