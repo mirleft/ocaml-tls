c25b24fc3e2270f1c2e711a8a147cdb6d3e54abc
Hannes Mehnert <hannes@mehnert.org>
2014-10-01 12:33:54 +0200

    add travis scripts

---
+++ .travis-ci.sh
@@ -0,0 +1,27 @@
+# OPAM packages needed to build tests.
+OPAM_PACKAGES="cstruct sexplib nocrypto x509 oUnit lwt mirage"
+
+case "$OCAML_VERSION" in
+    4.01.0) ppa=avsm/ocaml41+opam11 ;;
+    4.02.0) ppa=avsm/ocaml42+opam11 ;;
+    *) echo Unknown $OCAML_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+export OPAMYES=1
+
+opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
+
+opam pin nocrypto git://github.com/mirleft/ocaml-nocrypto.git
+opam pin asn1-combinators git://github.com/mirleft/ocaml-asn1-combinators.git
+opam pin x509 git://github.com/mirleft/ocaml-x509.git
+
+opam install ${OPAM_PACKAGES}
+
+eval `opam config env`
+ocaml setup.ml -configure --enable-tests --enable-lwt --enable-mirage
+ocaml setup.ml -build
+ocaml setup.ml -test
