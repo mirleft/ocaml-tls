82204565cbe1630ea7cb6b73faaca2daf326dd78
Thomas Leonard <talex5@gmail.com>
2014-11-21 10:08:13 +0000

    update Mirage example to work with Xen

--- mirage/example/config.ml
+++ mirage/example/config.ml
@@ -18,7 +18,10 @@ let net =
   try match Sys.getenv "NET" with
     | "direct" -> `Direct
     | _        -> `Socket
-  with Not_found -> `Socket
+  with Not_found ->
+    match get_mode () with
+    | `Unix -> `Socket
+    | `Xen -> `Direct
 
 let dhcp =
   try match Sys.getenv "ADDR" with
@@ -39,9 +42,13 @@ let client =
   foreign "Unikernel.Client" @@ console @-> stackv4 @-> entropy @-> kv_ro @-> job
 
 let () =
+  let platform =
+    match get_mode () with
+    | `Unix -> "unix"
+    | `Xen -> "xen" in
   (* Regrettably, CLOCK can't be dep-injected for now. *)
-  add_to_opam_packages [ "mirage-clock-unix" ] ;
-  add_to_ocamlfind_libraries [ "mirage-clock-unix"; "tls"; "tls.mirage" ] ;
+  add_to_opam_packages [ "mirage-clock-" ^ platform ] ;
+  add_to_ocamlfind_libraries [ "mirage-clock-" ^ platform; "tls"; "tls.mirage" ] ;
   match build with
   | `Server ->
       register "tls-server" [ server $ default_console $ stack default_console $ default_entropy $ disk ]
