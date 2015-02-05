1b0218d23b70f9cb0dc700f38108357724fdcb2a
Hannes Mehnert <hannes@mehnert.org>
2015-02-05 19:14:38 +0000

    update mirage for changes to tls errors

--- mirage/tls_mirage.ml
+++ mirage/tls_mirage.ml
@@ -5,7 +5,12 @@ module Make (F : V1_LWT.FLOW) (E : V1_LWT.ENTROPY) = struct
 
   module FLOW = F
 
-  type error  = [ `Tls of string | `Flow of FLOW.error ]
+  type error  = [
+    | `Tls of string
+    | `Tls_failure of Tls.Engine.failure
+    | `Tls_alert of Tls.Packet.alert_type
+    | `Flow of FLOW.error
+  ]
   type buffer = Cstruct.t
   type +'a io = 'a Lwt.t
 
@@ -30,7 +35,8 @@ module Make (F : V1_LWT.FLOW) (E : V1_LWT.ENTROPY) = struct
   }
 
   let tls_error e = `Error (`Tls e)
-  let tls_alert a = `Error (`Tls (Tls.Packet.alert_type_to_string a))
+  let tls_alert a = `Error (`Tls_alert a)
+  let tls_fail f  = `Error (`Tls_failure f)
 
   let return_error e     = return (`Error e)
   let return_tls_error e = return (tls_error e)
@@ -73,8 +79,8 @@ module Make (F : V1_LWT.FLOW) (E : V1_LWT.ENTROPY) = struct
             | `Ok _ -> return_unit
             | _     -> FLOW.close flow.flow ) >>
           return (`Ok data)
-      | `Fail (alert, `Response resp) ->
-          let reason = tls_alert alert in
+      | `Fail (fail, `Response resp) ->
+          let reason = tls_fail fail in
           flow.state <- reason ;
           FLOW.(write flow.flow resp >> close flow.flow) >> return reason
     in
