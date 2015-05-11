b0cd3e7ec063dbcdedb926422479525fa63371d9
Hannes Mehnert <hannes@mehnert.org>
2015-05-11 10:41:04 +0100

    build both examples

--- .travis-test-mirage.sh
+++ .travis-test-mirage.sh
@@ -5,4 +5,9 @@ eval `opam config env`
 opam install mirage
 
 cd mirage/example
+
+mirage clean && mirage configure && mirage build
 export BUILD=client && mirage clean && mirage configure && mirage build && ./mir-tls-client
+
+cd ../example2
+mirage clean && mirage configure && mirage build
