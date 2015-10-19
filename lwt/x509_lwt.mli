0377146a0d199f99cd5de0ea89ffd62f786928c1
Hannes Mehnert <hannes@mehnert.org>
2015-10-19 18:00:58 +0100

    adapt to X.509 master: support key pinning, remove certificate pinning

--- lwt/x509_lwt.mli
+++ lwt/x509_lwt.mli
@@ -24,8 +24,8 @@ val certs_of_pem_dir : Lwt_io.file_name -> X509.t list Lwt.t
 val authenticator :
   [ `Ca_file of Lwt_io.file_name
   | `Ca_dir  of Lwt_io.file_name
-  | `Fingerprints of Nocrypto.Hash.hash * (string * Cstruct.t) list
-  | `Hex_fingerprints of Nocrypto.Hash.hash * (string * string) list
+  | `Key_fingerprints of Nocrypto.Hash.hash * (string * Cstruct.t) list
+  | `Hex_key_fingerprints of Nocrypto.Hash.hash * (string * string) list
   | `No_authentication_I'M_STUPID ]
   -> authenticator Lwt.t
 
