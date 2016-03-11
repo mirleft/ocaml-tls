ae3d132d573a62c7f21b71632ee7c6705623c4c0
Azul <azul@riseup.net>
2016-03-11 21:01:27 +0000

    use the default clock to avoid get_mode

--- mirage/example/config.ml
+++ mirage/example/config.ml
@@ -14,21 +14,16 @@ let disk = generic_kv_ro secrets_dir
 let stack console = generic_stackv4 console tap0
 
 let server =
-  foreign ~deps:[abstract nocrypto] "Unikernel.Server" @@ console @-> stackv4 @-> kv_ro @-> job
+  foreign ~deps:[abstract nocrypto] "Unikernel.Server" @@ console @-> stackv4 @-> kv_ro @-> clock @-> job
 
 let client =
-  foreign ~deps:[abstract nocrypto] "Unikernel.Client" @@ console @-> stackv4 @-> kv_ro @-> job
+  foreign ~deps:[abstract nocrypto] "Unikernel.Client" @@ console @-> stackv4 @-> kv_ro @-> clock @-> job
 
 let () =
-  let platform =
-    match get_mode () with
-    | `Unix -> "unix"
-    | `Xen -> "xen" in
-  (* Regrettably, CLOCK can't be dep-injected for now. *)
-  add_to_opam_packages [ "mirage-clock-" ^ platform; "tls" ] ;
-  add_to_ocamlfind_libraries [ "mirage-clock-" ^ platform; "tls"; "tls.mirage" ] ;
+  add_to_opam_packages [ "tls" ] ;
+  add_to_ocamlfind_libraries [ "tls"; "tls.mirage" ] ;
   match build with
   | `Server ->
-      register "tls-server" [ server $ default_console $ stack default_console $ disk ]
+      register "tls-server" [ server $ default_console $ stack default_console $ disk $ default_clock ]
   | `Client ->
-      register "tls-client" [ client $ default_console $ stack default_console $ disk ]
+      register "tls-client" [ client $ default_console $ stack default_console $ disk $ default_clock ]
