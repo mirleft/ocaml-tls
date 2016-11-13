dee2bfc6c263208c760c6c7cc8d21b82a1b951ff
Hannes Mehnert <hannes@mehnert.org>
2016-11-13 12:58:07 +0000

    fix build script; test on 4.04

--- .travis-test-mirage.sh
+++ .travis-test-mirage.sh
@@ -6,8 +6,8 @@ opam install mirage
 
 cd mirage/example
 
-mirage clean && mirage configure -t unix --net=socket && mirage build
-export BUILD=client && mirage clean && mirage configure -t unix --net=socket && mirage build && ./mir-tls-client
+mirage clean && mirage configure -t unix --net=socket && make
+export BUILD=client && mirage clean && mirage configure -t unix --net=socket && make && ./mir-tls-client
 
 cd ../example2
-mirage clean && mirage configure && mirage build
+mirage clean && mirage configure && make
