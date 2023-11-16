f29a929cc26d72a14a47cdc80168bb4aae0c84d2
Hannes Mehnert <hannes@mehnert.org>
2023-11-16 20:16:50 +0100

    Update lib/handshake_crypto.mli
    Co-authored-by: Reynir Björnsson <reynir@reynir.dk>

--- lib/handshake_crypto.mli
+++ lib/handshake_crypto.mli
@@ -4,5 +4,6 @@ val derive_master_secret : Core.tls_before_13 -> session_data -> Cstruct.t -> Cs
 val initialise_crypto_ctx : Core.tls_before_13 -> session_data -> (crypto_context * crypto_context)
 val finished : Core.tls_before_13 -> Ciphersuite.ciphersuite -> Cstruct.t -> string -> Cstruct.t list -> Cstruct.t
 
+(** [pseudo_random_function version cipher length secret label seed] *)
 val pseudo_random_function : Core.tls_before_13 -> Ciphersuite.ciphersuite ->
   int -> Cstruct.t -> string -> Cstruct.t -> Cstruct.t
