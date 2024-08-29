8f1f5b1345883d92c3efa67bcc6ee3935f45c572
Hannes Mehnert <hannes@mehnert.org>
2024-08-29 11:03:39 +0200

    tls-lwt: read and write have an optional ?off argument

--- lwt/tls_lwt.ml
+++ lwt/tls_lwt.ml
@@ -153,12 +153,12 @@ module Unix = struct
             handle tls (String.sub (Bytes.unsafe_to_string t.recv_buf) 0 n)
           | `Closed -> Lwt.return `Eof
 
-  let rec read t buf =
+  let rec read t ?(off = 0) buf =
 
     let writeout res =
       let rlen = String.length res in
-      let n    = min (Bytes.length buf) rlen in
-      Bytes.blit_string res 0 buf 0 n ;
+      let n    = min (Bytes.length buf - off) rlen in
+      Bytes.blit_string res 0 buf off n ;
       t.linger <-
         (if n < rlen then Some (String.sub res n (rlen - n)) else None) ;
       Lwt.return n in
@@ -168,7 +168,7 @@ module Unix = struct
     | None     ->
         read_react t >>= function
           | `Eof           -> Lwt.return 0
-          | `Ok None       -> read t buf
+          | `Ok None       -> read t ~off buf
           | `Ok (Some res) -> writeout res
 
   let writev t css =
@@ -181,7 +181,11 @@ module Unix = struct
             ( t.state <- inject_state tls t.state ; write_t t tlsdata )
         | None -> Lwt.reraise @@ Invalid_argument "tls: write: socket not ready"
 
-  let write t cs = writev t [cs]
+  let write t ?(off = 0) cs =
+    let buf =
+      if off = 0 then cs else String.sub cs off (String.length cs - off)
+    in
+    writev t [buf]
 
   (*
    * XXX bad XXX
--- lwt/tls_lwt.mli
+++ lwt/tls_lwt.mli
@@ -51,12 +51,13 @@ module Unix : sig
 
   (** {2 Common stream operations} *)
 
-  (** [read t buffer] is [length], the number of bytes read into
-      [buffer]. *)
-  val read   : t -> bytes       -> int  Lwt.t
+  (** [read t ~off buffer] is [length], the number of bytes read into
+      [buffer]. It fills [buffer] starting at [off] (default is 0). *)
+  val read   : t -> ?off:int -> bytes -> int  Lwt.t
 
-  (** [write t buffer] writes the [buffer] to the session. *)
-  val write  : t -> string      -> unit Lwt.t
+  (** [write t ~off buffer] writes the [buffer] starting at [off] (default is 0)
+      to the session. *)
+  val write  : t -> ?off:int -> string -> unit Lwt.t
 
   (** [writev t buffers] writes the [buffers] to the session. *)
   val writev : t -> string list -> unit Lwt.t
